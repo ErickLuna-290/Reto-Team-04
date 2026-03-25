dae1b9c96b70c5e8227c197cb9b2cda719a9b5e5
ErickLuna-290 <a01563965@tec.mx>
2026-03-25 14:09:23 -0600

    Initial commit

---
+++ Assets/Scripts/CustomRequest/SensorInfo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SensorInfo
+{
+    public float temperatura;
+    public float humedad;
+
+    public List<Sensor> sensores;
+
+}
+
+[System.Serializable]
+
+public class Sensor
+{
+    public string nombre;
+}
